d1fad01b742065a44afae97c4165c5a760439243
Manikandan-Rajendran <r.manirajendran@gmail.com>
2023-02-10 00:38:11 +0530

    Initial Commit

---
+++ src/components/Expenses.js
@@ -0,0 +1,20 @@
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+
+const Expenses = (props) => {
+  return (
+    <div className="expenses">
+      {props.items.map((item) => {
+        return (
+          <ExpenseItem
+            date={item.date}
+            title={item.title}
+            amount={item.amount}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default Expenses;
